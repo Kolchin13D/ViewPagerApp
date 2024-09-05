9b6f3282dc155e1bbe0654b497d0cf58a0d1540a
Kolchin13D <kolchin13d@gmail.com>
2024-09-05 11:59:53 +0300

    testing

--- app/src/main/java/com/example/viewpagerapp/MainActivity.kt
+++ app/src/main/java/com/example/viewpagerapp/MainActivity.kt
@@ -5,16 +5,30 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.viewpager2.widget.ViewPager2
+import com.example.viewpagerapp.adapters.myAdapter
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var myAdapter: myAdapter
+    private val arrayList = ArrayList<String>()
+    lateinit var viewPager: ViewPager2
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        viewPager = findViewById(R.id.viewPager)
+        myAdapter = myAdapter(this)
+        viewPager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+        viewPager.adapter = myAdapter
+
     }
 }
